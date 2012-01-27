e0ed62dcdf853f7736d1088e7a3d50d5527f8884
Mike Valstar <mikevalstar@gmail.com>
2012-01-27 09:57:11 -0500

    Updated to use Crypto

--- addUser.js
+++ addUser.js
@@ -1,6 +1,15 @@
 /**********************
 A simple script to add a new user
 **********************/
+// Includes
+var crypto = require('crypto');
+
+function hashString(value) {
+  hash = crypto.createHash('sha1');
+  hash.update(value);
+  return hash.digest('hex');
+}
+
 // Database
 var Database = require('./lib/Database');
 var db = new Database();
@@ -8,10 +17,14 @@ db.connect('mongodb://localhost/mv');
 
 if(process.argv.length == 4){
 	// 0 will be node, 1 will be the script
-	db.addUser(process.argv[2], process.argv[3], function(err){
+	
+	var au = db.model('adminUser');
+	var usr = new au({login: process.argv[2], password: hashString(process.argv[3]) });
+	usr.save(function(err){
 		console.log("User added to database");
 		process.exit(0); // Success
 	});
+	
 }else{
 	console.error("Script requires exactly 2 arguments");
 	console.error("Usage: node addUser.js <email> <password>");
